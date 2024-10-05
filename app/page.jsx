7ac75b9ff3837a177d6e4ed06d4f79e59f3be564
mehdi hou <mehdi5juillet@gmail.com>
2024-10-05 23:24:46 +0200

    changes

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,6 @@ import Feed from "@/components/Feed";
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-      <img src="/assets/images/test.jpeg" alt="" />
       
       <h1 className="head_text text-center">
         Discover & Share
